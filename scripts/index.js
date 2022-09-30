00cf0c7c66324b282baac500a6e738b96e6f609f
Nataliya Bokareva <ns-bokareva@yandex.ru>
2022-09-30 12:12:52 +0300

    fix: исправлены комментарии по js и БЭМ

--- scripts/index.js
+++ scripts/index.js
@@ -3,28 +3,28 @@ const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
 const popupSaveButton = document.querySelector('.popup__save-button');
 
-let formElement = document.querySelector('.popup__form'); 
-let nameInput = document.querySelector('.popup__text_name');
-let jobInput = document.querySelector('.popup__text_job');
-let profileNameElement = document.querySelector('.profile__name');
-let profileDescriptionElement = document.querySelector('.profile__description');
+const formElement = document.querySelector('.popup__form'); 
+const nameInput = document.querySelector('.popup__text_type_name');
+const jobInput = document.querySelector('.popup__text_type_job');
+const profileNameElement = document.querySelector('.profile__name');
+const profileDescriptionElement = document.querySelector('.profile__description');
 
 const popupToggle = function() {
-    popup.classList.contains('popup_opened') 
-    nameInput.value = profileNameElement.textContent;
-    jobInput.value = profileDescriptionElement.textContent;
-    popup.classList.toggle('popup_opened');
+    if (!popup.classList.contains('popup_opened')) { 
+        nameInput.value = profileNameElement.textContent; 
+        jobInput.value = profileDescriptionElement.textContent; 
+    }
+    popup.classList.toggle('popup_opened'); 
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileNameElement.textContent = nameInput.value;
+    profileDescriptionElement.textContent = jobInput.value;
+    popupToggle();
 }
 
 popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
-
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    profileNameElement.textContent = nameInput.value
-    profileDescriptionElement.textContent = jobInput.value
-    popupToggle()
-}
 
 formElement.addEventListener('submit', formSubmitHandler);
