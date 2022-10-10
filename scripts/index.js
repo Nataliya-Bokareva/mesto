b08ccc131ea678ca735e3c6462fddf27eac10e1b
Nataliya Bokareva <ns-bokareva@yandex.ru>
2022-10-10 17:48:25 +0300

    feat:добавление карточек на стр, удаление

--- scripts/index.js
+++ scripts/index.js
@@ -12,11 +12,12 @@ const nameInput = document.querySelector('.popup__text_type_name');
 const jobInput = document.querySelector('.popup__text_type_job');
 const profileNameElement = document.querySelector('.profile__name');
 const profileDescriptionElement = document.querySelector('.profile__description');
-const likeButton = document.querySelector('.element__like-button');
+
 const template = document.querySelector(".template");
 const elementsContainer = document.querySelector(".elements");
-
-
+const placeNameInput = document.querySelector('.popup-new-place__text_type_name');
+const placeLinkInput = document.querySelector('.popup-new-place__text_type_link');
+const newFormElement = document.querySelector('.popup-new-place__form');
 
 
 const initialCards = [
@@ -51,6 +52,9 @@ const initialCards = [
       const newEl = createEl(initialCard.name, initialCard.link);
       elementsContainer.prepend(newEl);
     });
+
+    newpopupSaveButton.addEventListener('click', AddElement);
+    
   }
 
   const createEl = (name, link) => {
@@ -60,23 +64,30 @@ const initialCards = [
     newName.textContent = name;
     newImg.src = link;
     newImg.alt = name;
-    
+
+    newEl.querySelector('.element__like-button').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('element__like-button_active');
+   });
+
+   const deleteButton = newEl.querySelector(".element__trash-bin");
+   deleteButton.addEventListener("click", elDelete);
+
     return newEl;
   };
 
-  render();
+  const AddElement = (evt) => {
+    evt.preventDefault()
+    const Element = createEl(placeNameInput.value, placeLinkInput.value);
+    elementsContainer.prepend(Element);
+    placeNameInput.value = '';
+    placeLinkInput.value = '';
+    newpopupToggle();
+  };
 
- //function createElement (name, link) {
-    //const elTemplate = document.querySelector('.template').content;
-    //const newElement = elTemplate.querySelector('.element').cloneNode(true);
-    //newElement.querySelector('.element__image').src = initialCards.link;
-   // newElement.querySelector('.element__image').alt = initialCards.name;
-    //newElement.querySelector('.element__name').textContent = initialCards.name;
-    //elementsContainer.prepend(newElement);
-  //} 
-
-//createElement (initialCards);
-
+  const elDelete = (evt) => {
+    const currentEl = evt.target.closest('.element');
+    currentEl.remove();
+  }
 
 const popupToggle = function() {
     if (!popup.classList.contains('popup_opened')) { 
@@ -97,9 +108,18 @@ const newpopupToggle = function() {
     newpopup.classList.toggle('popup-new-place_opened'); 
 }
 
-//function likeToggle () {
-    //likeButton.classList.toggle('element__like-button_active');
+//const likeButton = document.querySelector('.element__like-button');
+   // likeButton.addEventListener('click', like);
+//const like = (evt) => {
+  //const likeEl = evt.target.classList.toggle('element__like-button_active');
 //}
+
+//function addLike () {
+  //likeButton.addEventListener('click', function (evt) {
+  //evt.target.classList.toggle('element__like-button_active');
+//});
+//}
+
 
 //songElement.querySelector('.song__like').addEventListener('click', function (evt) {
     //evt.target.classList.toggle('song__like_active');
@@ -109,6 +129,10 @@ popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
 newpopupOpenButton.addEventListener('click', newpopupToggle);
 newpopupCloseButton.addEventListener('click', newpopupToggle);
+//newpopupSaveButton.addEventListener('click', AddElement);
 //likeButton.addEventListener('click', likeToggle);
 
 formElement.addEventListener('submit', formSubmitHandler);
+//newFormElement.addEventListener('submit', AddElement);
+
+render();
